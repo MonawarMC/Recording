c82c8b84017b33913114368a188dea47c2d39cd0
bwfcwalshyPluginDev <bwfcwalshy@gmail.com>
2014-12-28 15:50:32 +0000

    Create Main.java

---
+++ com/bwfcwalshy/recording/Main.java
@@ -0,0 +1,16 @@
+package com.bwfcwalshy.recording;
+
+import java.util.ArrayList;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	ArrayList<Player> recording = new ArrayList<Player>();
+	
+	public void onEnable(){
+		getCommand("record").setExecutor(new Recording(this));
+	}
+	public void onDisable(){ recording.clear(); }
+}
