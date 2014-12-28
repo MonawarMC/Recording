76491ca9aba5a9fe62edb5b22c0e5df42e1391e6
bwfcwalshyPluginDev <bwfcwalshy@gmail.com>
2014-12-28 16:09:37 +0000

    Create Chat.java

---
+++ com/bwfcwalshy/recording/Chat.java
@@ -0,0 +1,36 @@
+package com.bwfcwalshy.recording;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+public class Chat implements Listener {
+
+	Main main;
+	public Chat(Main pl){
+		this.main = pl;
+	}
+	
+	@EventHandler
+	public void onChat(AsyncPlayerChatEvent e){
+		if(main.recording.contains(e.getPlayer())){
+			e.setCancelled(true);
+		}
+		for(Player p : main.recording){
+			if(e.getMessage().contains(p.getName())){
+				e.getPlayer().sendMessage(ChatColor.RED + "That player is recording and did not receive your message.");
+			}
+		}
+	}
+	@EventHandler
+	public void onMsg(PlayerCommandPreprocessEvent e){
+		for(Player p : main.recording){
+			if(e.getMessage().contains("/msg") && e.getMessage().contains(p.getName())){
+				e.setCancelled(true);
+			}
+		}
+	}
+}
